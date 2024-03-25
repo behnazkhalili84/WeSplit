910d3a4fc89f20ed1eea901940e75a6fa8085b8c
Behnaz <behnazkhalili84@gmail.com>
2024-03-24 20:57:24 -0400

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by behnaz Khalili on 2024-03-24.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
